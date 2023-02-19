baf7a65bb48130af4956b4db1f66f6716a6b98c7
Your Name <mrmofd@gmail.com>
2023-02-19 20:00:08 +0700

    One to one

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,22 +1,17 @@
 package org.example;
 
-import org.example.model.Movie;
-import org.example.model.Director;
+import org.example.model.Passport;
+import org.example.model.Person;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
-        Configuration configuration = new Configuration().addAnnotatedClass(Director.class).
-                addAnnotatedClass(Movie.class);
+        Configuration configuration = new Configuration().addAnnotatedClass(Person.class).addAnnotatedClass(Passport.class);
 
         SessionFactory sessionFactory = configuration.buildSessionFactory();
         Session session = sessionFactory.getCurrentSession();
@@ -24,13 +19,13 @@ public class App {
         try {
             session.beginTransaction();
 
-            Director director = new Director("Alya Kopaeva", 23);
+            Person person = new Person("Ivan", 34);
+            Passport passport = new Passport(34251367);
 
-            director.addMovie(new Movie("My sweet babysitter", 2003));
-            director.addMovie(new Movie("Kitchen", 2012));
-            director.addMovie(new Movie("Jopa popa", 2023));
+            person.setPassport(passport);
 
-            session.save(director);
+            session.save(person);
+
 
             session.getTransaction().commit();
         } finally {
